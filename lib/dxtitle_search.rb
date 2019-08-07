1c8eb473d3994c81430ddd540144408255618899
James Robertson <james@jamesrobertson.eu>
2019-08-07 11:30:57 +0100

    feature: Multiple Dynarex sources can now be read from a Dynarex file.

--- lib/dxtitle_search.rb
+++ lib/dxtitle_search.rb
@@ -7,25 +7,29 @@ require 'dynarex'
 
 class DxTitleSearch
 
-  def initialize(obj, debug: false)
+  def initialize(obj=nil, sources: nil, debug: false)
 
     @debug = debug
 
-    # is it a Dynarex file location?
-    s = if obj.lines.length < 2 then
+    s = if sources then
+    
+      dx = Dynarex.new(sources)
+      dx.all.map {|x| read x.uri }.join
+    
+    elsif obj then
+      
+      # is it a Dynarex file location?
+      if obj.lines.length < 2 then
 
-      xml = RXFHelper.read(obj).first
+        read obj
 
-      dx = Dynarex.new(xml)
-
-      dx.all.map do |x|
-       "%d %s %s" % [Time.parse(x.created).to_i, x.title, x.url]
-      end.join("\n")
-
-    else
-      obj
+      else
+        
+        obj
+        
+      end
     end
-
+  
     @h = h = s.lines.inject({}) do |r,x|
       key, value = x.split(/\s+(?=[^\s]+$)/,2)
       r.merge(key.rstrip => value)
@@ -36,21 +40,34 @@ class DxTitleSearch
   end
 
   def search(keywords)
+    
+    phrases = @a.grep /#{keywords}/i
 
     # find out the keywords count for each entry found
-    a = keywords.split.flat_map {|x| @a.grep /#{x}/i }.uniq.map do |entry|
+    a0 = keywords.split.flat_map do |x| 
+      next if @a.length < 2
+      @a.grep /#{x}/i 
+    end
+    
+    a = a0.uniq.map do |entry|
       [entry, entry.scan(/#{keywords.split.join('|')}/).uniq.count]
     end
 
     # sort by keywords found per entry and then date
+    #a2 = (phrases + a).uniq.sort do |x, x2|
     a2 = a.sort do |x, x2| 
       -([x.last, x.first[/^\d+/], ] <=> [x2.last, x2.first[/^\d+/]])
     end
 
     # format each result as a Hash object
-    a3 = a2.map do |x|
+    a3 = (phrases + a2).map do |x|
 
-      line, _ = x
+      if x.length > 1 then
+        line, _ = x
+      else
+        line = x
+      end
+      
       puts 'line: ' + line.inspect if @debug
      
       rawtime, title = line.split(/ +/,2)
@@ -65,6 +82,17 @@ class DxTitleSearch
     return a3
 
   end
+  
+  private
+  
+  def read(source)
+
+    dx = Dynarex.new(source)
+
+    dx.all.map do |x|
+      "%d %s %s" % [Time.parse(x.created).to_i, x.title, x.url]
+    end.join("\n")
+    
+  end
 
 end
-
