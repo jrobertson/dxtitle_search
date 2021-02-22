759e21bb2f24fc28e33cb40eb7464d8a6422c6ce
James Robertson <james@jamesrobertson.me.uk>
2021-02-22 12:16:08 +0000

    feature: Tags now be browsed and searched via recursive search.

--- dxtitle_search.gemspec
+++ dxtitle_search.gemspec
@@ -1,10 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'dxtitle_search'
-  s.version = '0.3.1'
+  s.version = '0.3.2'
   s.summary = 'Originally intended to search title entries from plain text (derived from a Dynarex document).'
   s.authors = ['James Robertson']
   s.files = Dir['lib/dxtitle_search.rb']
-  s.add_runtime_dependency('indexer101', '~> 0.2', '>=0.2.4')  
+  s.add_runtime_dependency('indexer101', '~> 0.2', '>=0.2.5')  
   s.signing_key = '../privatekeys/dxtitle_search.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/dxtitle_search.rb
+++ lib/dxtitle_search.rb
@@ -11,6 +11,7 @@ class DxTitleSearch
 
     @debug = debug
     @indexer = Indexer101.new debug: debug
+    @level = level
 
     s = if sources then
     
@@ -20,7 +21,7 @@ class DxTitleSearch
       puts 'a: ' + a.inspect if @debug
       @indexer.scan_dxindex a, level: level
     
-    elsif obj and obj.lines.length < 2 
+    elsif obj and (obj.is_a?(DxLite) or obj.is_a?(Dynarex)) or obj.lines.length < 2 
 
       @indexer.scan_dxindex  obj, level: level
         
@@ -43,9 +44,36 @@ class DxTitleSearch
 
     puts 'a3: ' + a3.inspect if @debug
     
+    @dx = Dynarex.new('results/result(title, url, date)').import(a3)
+    
     def a3.to_dx()
       Dynarex.new('results/result(title, url, date)').import(self)
     end
+    
+    def a3.to_tags()
+      a = self.map {|x| x[:title].scan(/(?<=#)(\w+)/)}.flatten
+      a.uniq.sort.map {|x| [x, a.count(x)]}
+    end    
+    
+    def a3.search(keywords)
+      
+      dx = Dynarex.new('results/result(title, url, date)').import(self)
+
+      level = keywords[0] == '#' ? 0 : 1
+      dts = DxTitleSearch.new dx, level: level
+      dts.search keywords
+      
+    end
+    
+    def a3.tag_search(keywords)
+      
+      dx = Dynarex.new('results/result(title, url, date)').import(self)
+
+      level = keywords[0] == '#' ? 0 : 1
+      dts = DxTitleSearch.new dx, level: level
+      dts.tag_search keywords
+      
+    end    
 
     return a3
 
@@ -54,6 +82,16 @@ class DxTitleSearch
   def tag_search(keywords)    
     r = @indexer.lookup *keywords.split(/[\W]+(?=[\w]+)/).map {|x| "#" + x}
     r.map {|x| x.to_s[1..-1]}
-  end  
+  end
+
+  def to_tags()
+    
+    a = @indexer.index.map do |key, value|
+      [key.to_s[1..-1], value.length]
+    end
+    
+    a.sort_by(&:first)
+    
+  end
 
 end
